d19e7b66a33f2f5f774260043e434f345040192b
Pandeyashish11 <125187343+Pandeyashish11@users.noreply.github.com>
2023-10-27 18:05:02 +0530

    Add files via upload
    Movie search app

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
-// Function to create a new blog post element
-function createBlogPost(title, content) {
-    const blogPost = document.createElement('div');
-    blogPost.className = 'blogPost';
-
-    const postTitle = document.createElement('h2');
-    postTitle.textContent = title;
-
-    const postContent = document.createElement('p');
-    postContent.textContent = content;
-
-    blogPost.appendChild(postTitle);
-    blogPost.appendChild(postContent);
-
-    return blogPost;
-    
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const moiveBox = document.querySelector("#movie-box")
+const getMovies = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    showMovies(data)
 }
-console.log("blogPost");
+getMovies(APIURL);
 
-// Function to handle the click event of the "+" button
-function handleAddButtonClick() {
-    const title = prompt('Enter Blog Post URL:');
-    const content = prompt('Enter Blog Title :');
 
-    if (title && content) {
-        const newBlogPost = createBlogPost(title, content);
-        const blogContainer = document.getElementById('blogContainer');
-        blogContainer.appendChild(newBlogPost);
+const showMovies = (data) => {
+    moiveBox.innerHTML = "";
+    data.results.forEach(
+        (result) => {
+            const imagePath = result.poster_path === null ? "img/image-missing.png" : IMGPATH + result.poster_path;
+            // const box = `
+            // <div class="box">
+            //     <img src="${IMGPATH+result}" alt="" />
+            //     <div class="overlay">
+            //         <h2>Overview:</h2>
+            //         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis iste doloribus quam voluptatum, illum unde nostrum dignissimos, mollitia, sapiente porro natus neque cupiditate distinctio quod possimus aliquid reiciendis vel. Soluta?
+            //     </div>
+            // </div>
+            // `
+            const box = document.createElement("div")
+            box.classList.add("box")
+            box.innerHTML = `
+                <img src="${imagePath}" alt="" />
+                <div class="overlay">
+                    <div class="title"> 
+                        <h2> ${result.original_title}  </h2>
+                        <span> ${result.vote_average} <span>
+                    </div>
+                    <h3>Overview:</h3>
+                    <p> 
+                        ${result.overview}
+                    </p>
+                 </div>
+            `
+            moiveBox.appendChild(box)
+        }
+    )
+}
+
+document.querySelector("#search").addEventListener(
+    "keyup",
+    function (event) {
+        if (event.target.value != "") {
+            getMovies(SEARCHAPI + event.target.value)
+        } else {
+            getMovies(APIURL);
+        }
     }
-}
-
-// Add event listener to the "+" button
-const addButton = document.getElementById('addButton');
-addButton.addEventListener('click', handleAddButtonClick);
-
-
-/*function daysInMonth(month, year) {
-    const numebrOfDays = new Date(year, month, 0).getDate();
-	return ("Number of days in " + month
-		+ "and month of the year " + year
-		+ " is " + numebrOfDays);
-}
-
-console.log(daysInMonth(2, 2023));
+)
